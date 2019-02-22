df3daf66469c578020baa4aabdbe4b4b84af4ce9
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-02-22 13:44:37 +0900

    add log

--- src/index.ts
+++ src/index.ts
@@ -53,7 +53,7 @@ export function addEvent<T extends Event>(
       return gen;
     })();
   if (Object.keys(event).includes(tag)) {
-    console.error("included tag");
+    console.error("included tag", tag);
   } else {
     event[tag] = func;
   }
