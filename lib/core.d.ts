3ee85d97683290b55ba8da3221f148d667142f35
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-07-23 17:36:45 +0900

    small

--- lib/core.d.ts
+++ lib/core.d.ts
@@ -22,8 +22,8 @@ export default class WebRTC {
     private pack;
     private event;
     onSignal: import("rx.mini").default<Signal>;
-    onConnect: import("rx.mini").default<unknown>;
-    onDisconnect: import("rx.mini").default<unknown>;
+    onConnect: import("rx.mini").default<undefined>;
+    onDisconnect: import("rx.mini").default<undefined>;
     onData: import("rx.mini").default<Message>;
     onAddTrack: import("rx.mini").default<MediaStream>;
     private wait4DC;
