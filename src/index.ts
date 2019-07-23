4908b2ce84ee64168daf0f30d93fc44e1c242ea9
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-07-23 17:30:55 +0900

    export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import WebRTC, { Signal } from "./core";
+import WebRTC, { Signal, Message } from "./core";
 import Stream from "./modules/stream";
 import FileShare from "./modules/file";
 import { getLocalVideo } from "./utill/media";
@@ -6,4 +6,4 @@ import { blob2Arraybuffer } from "./utill/arraybuffer";
 
 export default WebRTC;
 
-export { Stream, FileShare, getLocalVideo, blob2Arraybuffer, Signal };
+export { Stream, FileShare, getLocalVideo, blob2Arraybuffer, Signal, Message };
