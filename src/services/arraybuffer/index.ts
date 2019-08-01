69b7f7517420e260ebbd422e43389d0f602de44c
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-08-02 00:36:33 +0900

    done

--- src/services/arraybuffer/index.ts
+++ src/services/arraybuffer/index.ts
@@ -1,158 +1,91 @@
 import WebRTC from "../../core";
-import { mergeArraybuffer, sliceArraybuffer } from "../../utill/arraybuffer";
-import { decode, encode } from "@msgpack/msgpack";
-
-type State = {
-  done: number[];
-  index: number;
-  length: number;
-};
-const initialState: State = {
-  done: [],
-  index: 0,
-  length: 0
-};
-
-// TODO 再送制御
+import { sliceArraybuffer, mergeArraybuffer } from "../../utill/arraybuffer";
+import Event from "rx.mini";
 
 export default class ArrayBufferService {
-  label = "w4me_file";
-  private origin = "datachannel";
+  private label = "wrtc4me_abservice";
 
-  private memory: { [index: number]: ArrayBuffer } = {};
-  private state: State = initialState;
-  private sleep = 100;
+  private memory: ArrayBuffer[] = [];
 
-  listen(peer: WebRTC) {
-    peer.onData.subscribe(({ data, label }) => {
-      if (label === this.label) {
-        if (typeof data === "string") {
-          const { type, payload } = JSON.parse(data);
-          console.log({ type, payload });
-          if (type === "start") {
-            this.state.length = payload;
-          } else if (type === "end") {
-            const missing = [...Array(this.state.length)].reduce(
-              (arr, _, i) => {
-                if (!this.state.done.includes(i)) arr.push(i);
-                return arr;
-              },
-              [] as number[]
-            );
-            console.log({ missing });
-            if (missing.length === 0) {
-              const ab = mergeArraybuffer(Object.values(this.memory));
-              peer.onData.execute({
-                label: payload,
-                data: ab,
-                nodeId: peer.nodeId
-              });
-              peer.send(JSON.stringify({ type: "done", payload: "" }));
-              this.memory = [];
-            } else {
-              console.error("fail", this.state, Object.values(this.memory));
-              peer.send(
-                JSON.stringify({
-                  type: "missing",
-                  payload: missing
-                })
-              );
-            }
-            this.state = initialState;
-          }
-        } else {
-          const { i, v } = decode(data) as { i: number; v: Uint8Array };
-          this.state.done.push(i);
-          this.memory[i] = v;
-        }
-      }
-    });
+  private onData = new Event<MessageEvent>();
+
+  constructor(private peer: WebRTC) {
+    this.listen();
   }
 
-  // TODO test on kad
-  send = async (
-    ab: ArrayBuffer,
-    origin: string,
-    dc: RTCDataChannel,
-    peer: WebRTC
-  ) => {
-    this.origin = origin;
-    const chunks = sliceArraybuffer(ab, 64 * 1000);
-    console.log({ ab, chunks });
+  listen = async () => {
+    const { peer } = this;
 
-    const { unSubscribe } = peer.onData.subscribe(async ({ label, data }) => {
-      console.log("reply", { label, data });
-      const { type, payload } = JSON.parse(data as string) as {
-        type: string;
-        payload: number[];
-      };
-      if (type === "missing") {
-        if (dc.readyState === "closed" || dc.readyState === "closing") {
-          dc = await this.makeDC(peer.rtc);
-        }
+    await peer.onConnect.asPromise();
 
-        const missingChunks = payload.reduce(
-          (obj, index) => {
-            obj[index] = chunks[index];
-            return obj;
-          },
-          {} as {
-            [index: number]: ArrayBuffer;
-          }
-        );
-
-        await this.sendQuick(missingChunks, dc, peer.rtc);
-
-        dc.send(JSON.stringify({ type: "end", payload: origin }));
+    peer.onOpenDC.subscribe(dc => {
+      if (dc.label === this.label) {
+        dc.onmessage = this.onData.execute;
       }
-      if (type === "done") unSubscribe();
     });
 
-    dc.send(JSON.stringify({ type: "start", payload: chunks.length }));
-    await new Promise(r => setTimeout(r));
-    await this.sendQuick({ ...chunks }, dc, peer.rtc);
-    const res = await this.safeDC(dc, peer.rtc);
-    if (res) dc = res;
-    dc.send(JSON.stringify({ type: "end", payload: origin }));
+    this.onData.subscribe(ev => {
+      const { data } = ev;
+      if (typeof data === "string") {
+        const { type, payload } = JSON.parse(data);
+        switch (type) {
+          case "end": {
+            const ab = mergeArraybuffer(Object.values(this.memory));
+            peer.onData.execute({
+              label: payload,
+              data: ab,
+              nodeId: peer.nodeId
+            });
+            this.memory = [];
+          }
+        }
+      } else {
+        this.memory.push(data);
+      }
+    });
   };
 
-  private async sendQuick(
-    chunks: { [index: number]: ArrayBuffer },
-    dc: RTCDataChannel,
-    pc: RTCPeerConnection
-  ) {
-    let i = 0;
-    for (let index of Object.keys(chunks).map(v => Number(v))) {
-      if (dc.readyState === "open") {
-        try {
-          const chunk = chunks[index];
-          dc.send(encode({ i: index, v: new Uint8Array(chunk) }));
-          if (i % this.sleep === 0) await new Promise(r => setTimeout(r, 150));
-          i++;
-        } catch (error) {
-          console.log({ error });
-          this.sleep = parseInt(`${this.sleep / 2}`);
-          await new Promise(r => setTimeout(r, 150));
-        }
-      } else if (dc.readyState === "closed") {
-        dc = await this.makeDC(pc);
+  send = async (ab: ArrayBuffer, label: string) => {
+    const dc = await this.createDC();
+
+    const chunks = sliceArraybuffer(ab, 16 * 1000);
+
+    await this.job(chunks, dc);
+
+    this.rpc({ type: "end", payload: label }, dc);
+  };
+
+  private job = async (chunks: ArrayBuffer[], dc: RTCDataChannel) => {
+    for (let i = 0; i < chunks.length; ) {
+      if (dc.bufferedAmount > 1024 * 1000) {
+        await new Promise(r => setTimeout(r));
+        continue;
+      }
+      const chunk = chunks[i];
+      console.log({ i }, chunk);
+      try {
+        dc.send(chunk);
+        i++;
+      } catch (error) {
+        console.log({ error });
       }
     }
-  }
+  };
 
-  private async safeDC(dc: RTCDataChannel, pc: RTCPeerConnection) {
-    if (dc.readyState === "closed" || dc.readyState === "closing") {
-      return await this.makeDC(pc);
+  private rpc = async (
+    msg: { type: string; payload: any },
+    dc: RTCDataChannel
+  ) => {
+    try {
+      dc.send(JSON.stringify(msg));
+    } catch (error) {
+      console.log({ error });
     }
-  }
+  };
 
-  private async makeDC(pc: RTCPeerConnection) {
-    const dc = pc.createDataChannel(this.label);
-    await new Promise(resolve => {
-      dc.onopen = () => {
-        resolve();
-      };
-    });
+  private createDC = async () => {
+    const dc = this.peer.rtc.createDataChannel(this.label);
+    await new Promise(r => (dc.onopen = () => r()));
     return dc;
-  }
+  };
 }
