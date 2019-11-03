b8551e16a52a2fb6e4fbd270a9cb9b4f943bb965
shinyoshiaki <shinyoshiaki@users.noreply.github.com>
2019-11-03 09:22:30 +0000

    Automated deployment: Sun Nov  3 09:22:30 UTC 2019 5d80ecc8a293eb3793bb53c131287672ece38928

--- lib/modules/filequick.d.ts
+++
@@ -1,8 +0,0 @@
-import WebRTC from "../core";
-export default class SendFile {
-    private peer;
-    private label;
-    private blob?;
-    constructor(peer: WebRTC);
-    send(file: File): Promise<void>;
-}
--- lib/utill/testtools.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function Count(times: number, resolve: any): () => void;
